eb50daabca1077aae7e787f27baa601dc4df714e
ThePocketPotato <71907372+ThePocketPotato@users.noreply.github.com>
2023-05-08 23:24:49 -0700

    Update please.js

--- m05/please.js
+++ m05/please.js
@@ -13,18 +13,9 @@ function GetValueFromUser() {
 function GetTableContent(operator, table) {
   var i = 1;               
   var msg = '<h2>Multiplication Table</h2>';           
-  if (operator == 'addition') {
-    while (i < 11) {
-      var next_value = +i + +table;
-      msg += i + ' + ' + table + ' = ' + next_value + '<br>';
-      i++;
-    }
-  } else {
-    // Do multiplication
-    while (i < 11) {
-      msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';
-      i++;
-    }
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br>';
+     i++;
   }
   return msg;
 }
