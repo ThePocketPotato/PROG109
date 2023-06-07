2955f4255b5722b8da28757374afeee5b87ee393
ThePocketPotato <71907372+ThePocketPotato@users.noreply.github.com>
2023-06-07 14:45:16 -0700

    Create script.js

---
+++ Artifact05/script.js
@@ -0,0 +1,8 @@
+function add_item() {
+  var newItem = document.getElementById('items').value;
+  var newEl = document.createElement('li');
+  var newText = document.createTextNode(newItem);
+  newEl.appendChild(newText);
+  var position = document.getElementsByTagName('ul')[0];
+  position.appendChild(newEl);
+}
